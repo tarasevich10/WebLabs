b1208cb0d9e2946ffb8d5bec225323cb7e2474b7
tarasevich10 <32952473+tarasevich10@users.noreply.github.com>
2018-12-21 02:21:32 +0200

    Delete index.js

--- config/app/routes/index.js
+++
@@ -1,8 +0,0 @@
-const feedbacksRoutes = require('./feedbacks_routes');
-const newsRoutes = require('./news_routes');
-const pageRoutes = require('./page_routes');
-module.exports = function(app, db) {
-  feedbacksRoutes(app, db);
-  newsRoutes(app, db);
-  pageRoutes(app, db);
-};
